cd88b35eba94b149450a359ebaad9c6532617055
Sreedhar Joshi <joshi.sreedhar@gmail.com>
2015-11-04 14:12:44 +0530

    Integrated to console

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,6 +6,14 @@ namespace FizzBuzz
 	{
 		public static void Main (string[] args)
 		{
+			FizzBuzzGenerator fizzbizz = new FizzBuzzGenerator ();
+			InputParser parser = new InputParser ();
+			for (int i = 1; i < 100; i++) {
+				int value = parser.Parse (i.ToString ());
+				Console.Write(fizzbizz.GetToken (value));
+				Console.Write (", ");
+			}
+			Console.Read ();
 		}
 	}
 }
